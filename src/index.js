6f3547bfd23b7fb1cee625aeb9d449c8f4f7d2ab
Leo Kalshteyn <leo.kalshteyn@gmail.com>
2019-10-03 05:28:10 +0000

    Automatically backed up by Learn

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ document.addEventListener("DOMContentLoaded", () => {
   const form = document.getElementById("create-task-form");
   form.addEventListener("submit", function(e){
     let task = document.getElementById("new-task-description").value;
-    let new_el = document.createElement("li")
+    let li = document.createElement("li")
     new_el.textContent = task;
     const task_list = document.getElementById("tasks");
     taskList.appendChild(new_el);
